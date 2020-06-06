bdd9f8a0ae01925e0f650dc81b3e1d0267773163
sumihiro3 <sumihiro@gmail.com>
2020-06-06 10:09:49 +0900

    show all configs when app starts

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ config.dev = process.env.NODE_ENV !== 'production'
 
 // Show environment values
 consola.log('NODE_ENV', process.env.NODE_ENV)
-consola.log('BASE_URL', process.env.BASE_URL)
 consola.log('API_URL', process.env.API_URL)
 consola.log('USE_VCONSOLE', process.env.USE_VCONSOLE)
 consola.log('SKIP_LOGIN', process.env.SKIP_LOGIN)
@@ -23,14 +22,27 @@ consola.log(
   process.env.LINE_BOT_CHANNEL_ACCESS_TOKEN
 )
 consola.log('LINE_BOT_CHANNEL_SECRET', process.env.LINE_BOT_CHANNEL_SECRET)
+consola.log('LINE_PAY_CHANNEL_ID', process.env.LINE_PAY_CHANNEL_ID)
+consola.log('LINE_PAY_CHANNEL_SECRET', process.env.LINE_PAY_CHANNEL_SECRET)
 consola.log('KINTONE_DOMAIN_NAME', process.env.KINTONE_DOMAIN_NAME)
 consola.log('KINTONE_USER_ID', process.env.KINTONE_USER_ID)
 consola.log('KINTONE_USER_PASSWORD', process.env.KINTONE_USER_PASSWORD)
 consola.log(
+  'KINTONE_FOLLOWED_USER_APP_ID',
+  process.env.KINTONE_FOLLOWED_USER_APP_ID
+)
+consola.log('KINTONE_ORDER_ITEM_APP_ID', process.env.KINTONE_ORDER_ITEM_APP_ID)
+consola.log(
   'KINTONE_TRANSACTION_APP_ID',
   process.env.KINTONE_TRANSACTION_APP_ID
 )
-consola.log('KINTONE_ORDER_ITEM_APP_ID', process.env.KINTONE_ORDER_ITEM_APP_ID)
+consola.log('KINTONE_INQUIRY_APP_ID', process.env.KINTONE_INQUIRY_APP_ID)
+consola.log('LINE_RICH_MENU_DEFAULT_ID', process.env.LINE_RICH_MENU_DEFAULT_ID)
+consola.log('KINTONE_INQUIRY_APP_ID', process.env.KINTONE_INQUIRY_APP_ID)
+consola.log('USE_AZURE_AI', process.env.USE_AZURE_AI)
+consola.log('AZURE_TEXT_ANALYTICS_URL', process.env.AZURE_TEXT_ANALYTICS_URL)
+consola.log('AZURE_TEXT_ANALYTICS_KEY', process.env.AZURE_TEXT_ANALYTICS_KEY)
+consola.log('AZURE_TRANSLATOR_KEY', process.env.AZURE_TRANSLATOR_KEY)
 
 // LINE Bot
 const lineConfig = {
