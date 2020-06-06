44c41978add8585f5e4be8e096fda679ee66f241
sumihiro3 <sumihiro@gmail.com>
2020-06-06 10:10:10 +0900

    add script for set configs to heroku app

---
+++ set-configs-to-heroku.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# set configs to heroku loading environment values in .env file
+. .env
+heroku config:set API_URL=${API_URL}
+heroku config:set USE_VCONSOLE=${USE_VCONSOLE}
+heroku config:set SKIP_LOGIN=${SKIP_LOGIN}
+heroku config:set LIFF_ID=${LIFF_ID}
+heroku config:set LINE_BOT_CHANNEL_ACCESS_TOKEN=${LINE_BOT_CHANNEL_ACCESS_TOKEN}
+heroku config:set LINE_BOT_CHANNEL_SECRET=${LINE_BOT_CHANNEL_SECRET}
+heroku config:set LINE_PAY_CHANNEL_ID=${LINE_PAY_CHANNEL_ID}
+heroku config:set LINE_PAY_CHANNEL_SECRET=${LINE_PAY_CHANNEL_SECRET}
+heroku config:set KINTONE_DOMAIN_NAME=${KINTONE_DOMAIN_NAME}
+heroku config:set KINTONE_USER_ID=${KINTONE_USER_ID}
+heroku config:set KINTONE_USER_PASSWORD=${KINTONE_USER_PASSWORD}
+heroku config:set KINTONE_FOLLOWED_USER_APP_ID=${KINTONE_FOLLOWED_USER_APP_ID}
+heroku config:set KINTONE_ORDER_ITEM_APP_ID=${KINTONE_ORDER_ITEM_APP_ID}
+heroku config:set KINTONE_TRANSACTION_APP_ID=${KINTONE_TRANSACTION_APP_ID}
+heroku config:set KINTONE_INQUIRY_APP_ID=${KINTONE_INQUIRY_APP_ID}
+heroku config:set LINE_RICH_MENU_DEFAULT_ID=${LINE_RICH_MENU_DEFAULT_ID}
+heroku config:set USE_AZURE_AI=${USE_AZURE_AI}
+heroku config:set AZURE_TEXT_ANALYTICS_URL=${AZURE_TEXT_ANALYTICS_URL}
+heroku config:set AZURE_TEXT_ANALYTICS_KEY=${AZURE_TEXT_ANALYTICS_KEY}
+heroku config:set AZURE_TRANSLATOR_KEY=${AZURE_TRANSLATOR_KEY}
+
+# show configs
+heroku config
